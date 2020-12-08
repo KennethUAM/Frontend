a8a3884ff782b0f657d5d7df90cd42459efe94e7
HIDALGO LOAIZA <kenett.hidalgo@uamcr.net>
2020-12-07 18:33:47 -0600

    Creada la clase categoria clientes

---
+++ DAL/MantenimientosCategoriaCliente_DAL/CatClienteDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.MantenimientosCategoriaCliente_DAL
+{
+    public class CatClienteDAL
+    {
+        #region
+        private String categoria;
+
+
+        #endregion
+        public string Categoria { get => categoria; set => categoria = value; }
+
+    }
+}
